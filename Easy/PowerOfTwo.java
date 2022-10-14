f79d9125378efcacd58316a2a6db53a90e5d2d36
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-10-14 22:23:34 +0530

    Create PowerOfTwo.java

---
+++ Easy/PowerOfTwo.java
@@ -0,0 +1,22 @@
+// recursive approch
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1){
+            return true;
+        }
+        else if (n % 2 != 0 || n == 0){
+            return false;
+        }
+        return isPowerOfTwo(n/2);
+    }
+}
+
+
+//Bit manipulation
+
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         return n > 0 && (n & n - 1) == 0;
+//     }
+// }
