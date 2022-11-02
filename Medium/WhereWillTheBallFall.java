63a03539f3503fb5b809983124ffab3a601ce4bf
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-11-02 16:41:57 +0530

    Update WhereWillTheBallFall.java

--- Medium/WhereWillTheBallFall.java
+++ Medium/WhereWillTheBallFall.java
@@ -1,14 +1,14 @@
 class Solution {
     
     public int dfs(int i, int j, int[][] grid){
-        if(i >= grid.length){ //base case
+        if(i >= grid.length){                                               //base case
             return j;
         }
         
         if(grid[i][j] == 1 && j + 1 < grid[0].length && grid[i][j+1] == 1){ //left to right
             return dfs(i+1,j+1,grid);
         } 
-        else if(grid[i][j] == -1 && j - 1 >= 0 && grid[i][j-1] == -1){ //right to left
+        else if(grid[i][j] == -1 && j - 1 >= 0 && grid[i][j-1] == -1){.    //right to left
             return dfs(i+1,j-1,grid);
         } else {
             return -1;
