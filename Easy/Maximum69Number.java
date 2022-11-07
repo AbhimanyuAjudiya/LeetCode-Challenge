bea73e0d65c0d18828d1ac34e990a34240144f5d
Abhimanyu Ajudiya <88022199+AbhimanyuAjudiya@users.noreply.github.com>
2022-11-07 21:04:10 +0530

    Update Maximum69Number.java

--- Easy/Maximum69Number.java
+++ Easy/Maximum69Number.java
@@ -1,5 +1,17 @@
 class Solution {
     public int maximum69Number (int num) {
-        return Integer.parseInt(String.valueOf(num).replaceFirst("6","9"));
+        int temp = num;
+        int i = 0;
+        int idx = 0;
+        while(temp > 0){
+            i++;
+            int digit = temp % 10;
+            temp /= 10;
+            if(digit == 6){
+                idx = i;
+            } 
+        }
+        return num + 3 * (int)Math.pow(10, idx - 1);
+        // return Integer.parseInt(String.valueOf(num).replaceFirst("6","9"));
     }
 }
