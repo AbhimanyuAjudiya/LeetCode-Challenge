c7890645413d2c24678b23126d0887a1558501ae
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-02-05 22:21:18 +0530

    Update PermutationInString.java

--- Medium/PermutationInString.java
+++ Medium/PermutationInString.java
@@ -1,30 +1,28 @@
 public class Solution {
-    boolean flag = false;
-    
     public boolean checkInclusion(String s1, String s2) {
-        permute(s1, s2, 0);
-        return flag;
-    }
-    
-    public String swap(String s, int i0, int i1) {
-        if (i0 == i1)
-            return s;
-        String s1 = s.substring(0, i0);
-        String s2 = s.substring(i0 + 1, i1);
-        String s3 = s.substring(i1 + 1);
-        return s1 + s.charAt(i1) + s2 + s.charAt(i0) + s3;
-    }
-    
-    void permute(String s1, String s2, int l) {
-        if (l == s1.length()) {
-            if (s2.indexOf(s1) >= 0)
-                flag = true;
-        } else {
-            for (int i = l; i < s1.length(); i++) {
-                s1 = swap(s1, l, i);
-                permute(s1, s2, l + 1);
-                s1 = swap(s1, l, i);
+        if (s1.length() > s2.length())
+            return false;
+        HashMap<Character, Integer> s1map = new HashMap<>();
+
+        for (int i = 0; i < s1.length(); i++)
+            s1map.put(s1.charAt(i), s1map.getOrDefault(s1.charAt(i), 0) + 1);
+
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            HashMap<Character, Integer> s2map = new HashMap<>();
+            for (int j = 0; j < s1.length(); j++) {
+                s2map.put(s2.charAt(i + j), s2map.getOrDefault(s2.charAt(i + j), 0) + 1);
             }
+            if (matches(s1map, s2map))
+                return true;
         }
+        return false;
+    }
+
+    public boolean matches(HashMap<Character, Integer> s1map, HashMap<Character, Integer> s2map) {
+        for (char key : s1map.keySet()) {
+            if (s1map.get(key) - s2map.getOrDefault(key, -1) != 0)
+                return false;
+        }
+        return true;
     }
 }
