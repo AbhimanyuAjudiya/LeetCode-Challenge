7cf82437df4f434ab545da578e346c50a2946961
Abhimanyu Ajudiya <abhimanyuajudiya@gmail.com>
2023-05-16 02:44:59 +0530

    Create SwappingNodesInALinkedList.java

---
+++ Medium/SwappingNodesInALinkedList.java
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode swapNodes(ListNode head, int k) {
+        ListNode l=head;
+        ListNode r=head;
+        for(int i=1;i<k;i++)
+            l=l.next;
+        ListNode curr = l;
+        while(curr.next!=null){
+            curr = curr.next;
+            r=r.next;
+        }
+        int t = l.val;
+        l.val = r.val;
+        r.val =t;
+        return head;
+    }
+}
